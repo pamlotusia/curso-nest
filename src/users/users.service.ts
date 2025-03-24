e5454de47939efbfaa265008d85ab99afd1680e4
Pamela Lima <pamela.jesus.lima@gmail.com>
2025-03-24 11:48:21 -0300

    salvando antes de almoçar :)

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UsersService {
+  constructor(private readonly prisma: PrismaService){}
   
+  findAll(){
+    return "Retornando serviço"
+  }
 }
