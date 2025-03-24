5ab125edf68760508b63b0bcfd46cedf5f33a7e7
Pamela Lima <pamela.jesus.lima@gmail.com>
2025-03-24 13:40:48 -0300

    salvo

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,13 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
-
-
 @Controller('users')
 export class UsersController {
   constructor(private readonly userService: UsersService){}
 
   @Get()
   FindAllUsers(){
-    return this.userService.findAll()
+    return 'foi'
   }
 }
